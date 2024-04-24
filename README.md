3769f26c808d4be95279153e20f50366a478e70e
ja-yy <jaypatel.learn@gmail.com>
2024-04-24 17:03:59 +0530

    Done: Working Bot

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Invoice-excreter-bot-llm
-Streamlit app leveraging OpenAI's LLM for accurate invoice extraction, supporting PDF files and offering JSON/CSV outputs.
+
+### 📄Project description
+
+Welcome to my Streamlit app designed for invoice extraction utilizing OpenAI's API. This versatile tool offers two distinct output models for enhanced flexibility and accuracy in extracting invoice data.
+
+### 🎯 Purpose and Achievement:
+
+Acknowledging the widespread demand for data extraction solutions utilizing LLM, and inspired by Langchain's introduction of the `with_structured_output` method for enhanced model calls, I centered my efforts on tackling the common use case of invoice extraction, culminating in the development of this bot.
+
+### ⚙️🚀 Technologies and Frameworks Utilized
+
+![Python](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)
+![Streamlit](https://img.shields.io/badge/Streamlit-FF4B4B?style=for-the-badge&logo=Streamlit&logoColor=white)
+![OpenAI](https://img.shields.io/badge/OpenAI-black?style=for-the-badge)
+![Langchain](https://img.shields.io/badge/%F0%9F%A6%9C%EF%B8%8F%F0%9F%94%97%20LangChain-black?style=for-the-badge)
+
+
+### 💡Features
+
+- Supports extraction of data from `PDF` files
+- Utilizes `function calling` with LLM to enhance accuracy
+- Offers two output options: `JSON` and `CSV`
+
+### 🔜 Future Development and Features
+
+In upcoming iterations, I aim to incorporate the ability to extract data from Excel and Word files, expanding the application's versatility and utility.
+
+## ▶️ Run
+
+Clone the repository
+
+```bash
+git clone git@github.com:Ja-yy/Invoice-excreter-bot-llm.git
+```
+
+Create a virtual environment using Pipenv:
+
+```bash
+pipenv install
+```
+
+Start Streamlit server:
+```bash
+streamlit run main.py 
+```
+
+Now, go to [localhost:8501](http://localhost:8501/)
+
+Enjoy the app :)
---
+++ extracter_agent.py
@@ -0,0 +1,66 @@
+import shutil
+import tempfile
+from typing import List
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts.chat import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+from pydantic.v1 import BaseModel
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+
+class Invoice(BaseModel):
+
+    class Items(BaseModel):
+        description: str
+        quantity: int
+        unit_price: float
+        amount: float
+
+    invoiceID: str
+    date: str
+    items: List[Items]
+    tax_rate: str
+    total_amount: float
+
+
+class ExtracterAgent:
+
+    output_parser: PydanticOutputParser = PydanticOutputParser(pydantic_object=Invoice)
+
+    def parse_pdf_input(self, uploaded_files: UploadedFile):
+        parded_pdf = []
+        for file in uploaded_files:
+            with tempfile.NamedTemporaryFile(delete=False) as tmp:
+                shutil.copyfileobj(file, tmp)
+            pdf_loader = PyPDFLoader(tmp.name)
+            pdf_reader = pdf_loader.load()
+            parded_pdf.append(pdf_reader)
+        return parded_pdf
+
+    def create_prompt(self):
+        sys_template = """You are expert at data extraction.
+You tasked to extracted data from given tax invoice.
+"""
+        prompt = ChatPromptTemplate.from_messages(
+            [("system", sys_template), ("human", "{pdf_input}")]
+        ).partial(format_instruction=self.output_parser.get_format_instructions())
+        return prompt
+
+    def create_chain(self):
+        llm = ChatOpenAI(model="gpt-3.5-turbo")
+        prompt = self.create_prompt()
+        chain = (
+            {"pdf_input": RunnablePassthrough()}
+            | prompt
+            | llm.with_structured_output(schema=Invoice)
+        )
+        return chain
+
+    def run_agent(self, pdf):
+        loaded_file = self.parse_pdf_input(pdf)
+        chain = self.create_chain()
+        llm_res = chain.batch(loaded_file)
+        return llm_res
